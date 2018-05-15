8f042d301703d4c675ec849ff26372b4eb2ea714
n4cl <devn4cl@gmail.com>
2018-05-15 20:53:39 +0900

    Add ITP1_1_C.cpp

---
+++ ITP1/ITP1_1_C.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int a, b, area, permeter;
+    cin >> a >> b;
+    area = a * b;
+    permeter = 2 * a + 2 * b;
+    printf("%d %d\n", area, permeter);
+    return 0;
+}
