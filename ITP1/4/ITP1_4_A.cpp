de59b73e8957579cb3a16e97a995d138117793c1
n4cl <devn4cl@gmail.com>
2018-05-17 17:49:43 +0900

    Add ITP1_4_A.cpp

---
+++ ITP1/4/ITP1_4_A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int a, b;
+    cin >>a >>b;
+    printf("%d\n", a / b);
+    printf("%d\n", a % b);
+    printf("%f\n", (double)a / (double)b);
+    return 0;
+}
