ecdfada63cfa367eda7e8f1ae3dadbd18d17a920
n4cl <devn4cl@gmail.com>
2018-05-19 00:46:21 +0900

    Add ITP1_4_B.cpp

---
+++ ITP1/4/ITP1_4_B.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    double r;
+    double pi = acos(-1.0);
+    cin >>r;
+    printf("%f %f\n", pi * pow(r, 2), 2 * pi * r);
+    return 0;
+}
