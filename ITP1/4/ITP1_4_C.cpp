2bb4dc3cc9c6174e1d1a9a185634b19e682ebc23
n4cl <devn4cl@gmail.com>
2018-05-18 17:50:38 +0900

    Add ITP1_4_C.cpp

---
+++ ITP1/4/ITP1_4_C.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int a, b;
+    string op;
+    while(1){
+        cin >> a >> op >> b;
+        if(op == "+"){
+            cout << a + b << endl;
+        }else if(op == "-"){
+            cout << a - b << endl;
+        }else if(op == "*"){
+            cout << a * b << endl;
+        }else if(op == "/"){
+            cout << a / b << endl;
+        }else if(op == "?"){
+            break;
+        }
+    }
+    return 0;
+}
